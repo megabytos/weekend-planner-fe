149b506a4770834149fe0bfe8a0961f94f4f8bff
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-11-04 00:28:54 +0000

    ESLint and Prettier were configured

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+// @ts-ignore
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  'prettier',
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
---
+++ lint-staged.config.cjs
@@ -0,0 +1,6 @@
+const path = require('path');
+const formatCommand = 'prettier . --write';
+
+module.exports = {
+  '*': formatCommand,
+};
