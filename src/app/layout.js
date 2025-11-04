1002f82abd19ebbe93504a46d8f23343a0757ca3
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-11-04 00:00:48 +0000

    TanStack Query was added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/libs/redux-provider";
@@ -12,10 +16,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Weekend Planner",
-  description: "Plan your weekend activities",
-};
+const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
   return (
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReduxProvider>{children}</ReduxProvider>
+        <QueryClientProvider client={queryClient}>
+          <ReduxProvider>{children}</ReduxProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { add } from "@/libs/counterSlice";
 import { useAppDispatch } from "@/libs/hooks/use-app-dispatch";
 import { useAppSelector } from "@/libs/hooks/use-app-selector";
@@ -7,6 +9,20 @@ import { useAppSelector } from "@/libs/hooks/use-app-selector";
 export default function Home() {
   const counter = useAppSelector((state) => state.counter.counter);
   const dispatch = useAppDispatch();
+
+  const queryClient = useQueryClient();
+
+  const query = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => (await fetch("/api/users")).json(),
+  });
+
+  // Mutations
+  const mutation = useMutation({
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
 
   return (
     <div>
