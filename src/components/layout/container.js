d7a8116d3e8454e53d99f7e5890a6ec0b28b9d25
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-11-06 16:46:16 +0000

    cn util and respective libs were added

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -1,5 +1,14 @@
-'use client';
+import cn from '@/utils/class-names';
 
-export default function Container({ children }) {
-  return <div className="container mx-auto px-4">{children}</div>;
+export default function Container({ className, children }) {
+  return (
+    <div
+      className={cn(
+        'w-full max-w-c[var(--container-max-width)] mx-auto px-5 lg:px-8 xl:px-0',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
 }
