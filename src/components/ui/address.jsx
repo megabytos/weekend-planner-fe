bca098519806a50f58e2361ba4e11fafc1f78c24
Artur Didur <didur.art@gmail.com>
2025-11-09 19:52:33 +0100

    ftr address

---
+++ src/components/ui/address.jsx
@@ -0,0 +1,12 @@
+import { MapPin } from 'lucide-react';
+
+export default function Address({ address }) {
+  return (
+    <div className="h-5 flex items-center justify-start gap-2">
+      <MapPin className="w-3.5 h-5 text-black aria-hidden" />
+      <address className="not-italic text-blue text-sm leading-5">
+        {address}
+      </address>
+    </div>
+  );
+}
