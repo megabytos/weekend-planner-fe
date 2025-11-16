817c4e4795da573cf482fc1fb3d7adda806b0300
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-11-16 18:47:40 +0000

    sign-in and signup pages were added

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -8,14 +8,14 @@ import ButtonMain from '@/components/ui/button-main';
 import InputBase from '@/components/ui/input/input-base';
 import InputPassword from '@/components/ui/input/input-password';
 
-export default function SignInPage() {
-  const handleSignIn = () => {
-    // Handle sign-in logic here
+export default function SignUpPage() {
+  const handleSignUp = () => {
+    // Handle sign-up logic here
   };
 
   return (
     <Container className="pt-12 pb-16 md:py-20">
-      <h1 className="text-[22px] text-center">Sign In</h1>
+      <h1 className="text-[22px] text-center">Sign Up</h1>
       <form className="mt-6 flex flex-col gap-4 max-w-[384px] mx-auto">
         <InputBase label="Email" />
         <InputPassword label="Password" />
@@ -23,11 +23,11 @@ export default function SignInPage() {
           href="#"
           className="flex gap-1 justify-end items-center text-sm text-blue text-right"
         >
-          <span>Forgot password?</span>
+          <span>Already have an account?</span>
           <ArrowRight size={16} />
         </Link>
-        <ButtonMain onClick={handleSignIn} className="mx-auto max-w-[384px]">
-          Sign In
+        <ButtonMain onClick={handleSignUp} className="mx-auto max-w-[384px]">
+          Sign Up
         </ButtonMain>
       </form>
     </Container>
